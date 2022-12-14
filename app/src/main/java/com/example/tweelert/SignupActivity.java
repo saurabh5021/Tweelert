0e01bd7677e185478386332cc2d8f958c15c9631
blayush <ayushmishra42@gmail.com>
2022-12-15 00:42:37 +0530

    splash screeen+ui enhanced

--- app/src/main/java/com/example/tweelert/SignupActivity.java
+++ app/src/main/java/com/example/tweelert/SignupActivity.java
@@ -2,13 +2,31 @@ package com.example.tweelert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignupActivity extends AppCompatActivity {
+
+    EditText email,password;
+    Button signupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        signupBtn = findViewById(R.id.signUpbutton);
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/tweelert/SplashScreen.java
+++ app/src/main/java/com/example/tweelert/SplashScreen.java
@@ -25,7 +25,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                Intent intent = new Intent(SplashScreen.this, SignupActivity.class);
                 startActivity(intent);
                 finish();
             }
