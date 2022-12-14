a96bfee4f1d30bf94509dbf4ec3abf600ded7d4e
blayush <ayushmishra42@gmail.com>
2022-12-15 01:36:23 +0530

    login activity updated

--- app/src/main/java/com/example/tweelert/LoginActivity.java
+++ app/src/main/java/com/example/tweelert/LoginActivity.java
@@ -3,12 +3,21 @@ package com.example.tweelert;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextView signupTxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        signupTxt=findViewById(R.id.textView3);
+        signupTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/tweelert/SplashScreen.java
+++ app/src/main/java/com/example/tweelert/SplashScreen.java
@@ -38,7 +38,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, SignupActivity.class);
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
