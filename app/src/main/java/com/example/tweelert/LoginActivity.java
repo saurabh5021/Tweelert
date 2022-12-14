55d8442d942e32a54609ac4ad4f7ea3ac973cf2d
Saurabh <saurabhb5021@gmail.com>
2022-12-15 02:16:23 +0530

    Implemented Login and Registration

--- app/src/main/java/com/example/tweelert/LoginActivity.java
+++ app/src/main/java/com/example/tweelert/LoginActivity.java
@@ -1,23 +1,80 @@
 package com.example.tweelert;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-    TextView signupTxt;
+
+    private FirebaseAuth mAuth;
+    private EditText email, password;
+    private Button btnLogin;
+    private TextView textRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        signupTxt=findViewById(R.id.textView3);
-        signupTxt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.emaileditText);
+        password = findViewById(R.id.passwordeditText);
+        btnLogin  = findViewById(R.id.loginbutton2);
+        textRegister = findViewById(R.id.signupbutton);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
             }
         });
+
+        textRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+            }
+        });
+    }
+
+    private void login()
+    {
+        String user = email.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+        if(user.isEmpty())
+        {            email.setError("Email can not be empty");        }
+        if(pass.isEmpty())
+        {            password.setError("Password can not be empty");        }
+        else
+        {
+            mAuth.signInWithEmailAndPassword(user,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task)
+                {
+                    if(task.isSuccessful())
+                    {
+                        Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(LoginActivity.this , MainActivity.class));
+                    }
+                    else
+                    {
+                        Toast.makeText(LoginActivity.this, "Login Failed"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
--- app/src/main/java/com/example/tweelert/SignupActivity.java
+++ app/src/main/java/com/example/tweelert/SignupActivity.java
@@ -1,5 +1,6 @@
 package com.example.tweelert;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,26 +8,75 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     EditText email,password;
-    Button signupBtn;
+    Button signupBtn, signinBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.emaileditText);
+        password = findViewById(R.id.passwordeditText);
         signupBtn = findViewById(R.id.loginbutton);
+        signinBtn = findViewById(R.id.signinbutton);
+
+        signinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+            }
+        });
 
         signupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
-                startActivity(intent);
+                Register();
             }
         });
 
+    }
+
+    private void Register()
+    {
+        String user = email.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+        if(user.isEmpty())
+        {
+            email.setError("Email can not be empty");
+        }
+        if(pass.isEmpty())
+        {
+            password.setError("Password can not be empty");
+        }
+        else
+        {
+            mAuth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        Toast.makeText(SignupActivity.this, "User registered successfully", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(SignupActivity.this, MainActivity.class));
+                    }
+                    else
+                    {
+                        Toast.makeText(SignupActivity.this, "Registration Failed"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
+        }
     }
 }
