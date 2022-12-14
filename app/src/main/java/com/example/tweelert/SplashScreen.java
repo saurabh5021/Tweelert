76b15aa2561f5c98f44613fdd160bbfb21735adb
Saurabh <saurabhb5021@gmail.com>
2022-12-14 23:27:48 +0530

    Added Splash Screen

---
+++ app/src/main/java/com/example/tweelert/SplashScreen.java
@@ -0,0 +1,44 @@
+package com.example.tweelert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Animation topAnim, bottomAnim;
+    ImageView logo;
+    TextView title, tag;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+        logo = findViewById(R.id.logo);
+        title = findViewById(R.id.title);
+        tag = findViewById(R.id.tagline);
+
+        logo.setAnimation(topAnim);
+        title.setAnimation(bottomAnim);
+        tag.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2500);
+
+    }
+}
