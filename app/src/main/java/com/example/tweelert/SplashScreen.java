261d74494d7892e0c418d24c001f9df0c0673017
Saurabh <saurabhb5021@gmail.com>
2022-12-15 00:40:28 +0530

    some splash screen changes

--- app/src/main/java/com/example/tweelert/SplashScreen.java
+++ app/src/main/java/com/example/tweelert/SplashScreen.java
@@ -14,12 +14,25 @@ public class SplashScreen extends AppCompatActivity {
 
     Animation topAnim, bottomAnim;
     ImageView logo;
-    TextView title, tag;
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        Animation a1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.blink);
+
+
+        logo = findViewById(R.id.imageView3);
+        title = findViewById(R.id.textView5);
+
+        //logo.setAnimation(topAnim);
+        title.setAnimation(bottomAnim);
+        logo.startAnimation(a1);
 
 
         new Handler().postDelayed(new Runnable() {
